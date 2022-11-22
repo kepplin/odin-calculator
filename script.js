3501a35686a4270c4a6ef90735d39e81f375c766
Pranav Borse <pranavborse91@gmail.com>
2022-11-22 17:38:47 +1100

    Create evaluation functions

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 const prevDisplay = document.querySelector('.prevDisplay');
 const currentDisplay = document.querySelector('.currentDisplay');
-const inputButton = document.querySelectorAll('.inputButton')
+const inputButton = document.querySelectorAll('.inputButton');
+const divideButton = document.querySelector('.divideButton');
+let result = '';
 
 for(let i = 0; i < inputButton.length; i++){
     inputButton[i].addEventListener('click', function(){
-        currentDisplay.textContent = inputButton[i].getAttribute("data-num");
-        prevDisplay.textContent = inputButton[i].getAttribute("data-num");
+        result = result + inputButton[i].getAttribute("data-num");
+        currentDisplay.textContent = result;
+        prevDisplay.textContent = result;
     })
+    
+}
+
+function calcPercent(num1, num2){
+    return num1/100 * num2
+}
+function calcDivide(num1, num2){
+    return num1 / num2
+}
+function calcMultiply(num1, num2){
+    return num1 * num2
+}
+function calcSubtract(num1, num2){
+    return num1 - num2
+}
+function calcAdd(num1, num2){
+    return num1 + num2
+}
+function operate(num1, num2){
+    
 }
