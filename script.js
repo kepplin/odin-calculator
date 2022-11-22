80af61b4dfdb778bc91891e0e6785b8fca78bab2
Pranav Borse <pranavborse91@gmail.com>
2022-11-22 17:45:14 +1100

    Create operate function

--- script.js
+++ script.js
@@ -28,6 +28,20 @@ function calcSubtract(num1, num2){
 function calcAdd(num1, num2){
     return num1 + num2
 }
-function operate(num1, num2){
-    
-}
+function operate(operator, num1, num2) {
+    num1 = Number(num1)
+    num2 = Number(num2)
+    switch (operator) {
+      case '+':
+        return calcAdd(num1, num2)
+      case '-':
+        return calcSubtract(num1, num2)
+      case '×':
+        return calcMultiply(num1, num2)
+      case '÷':
+        if (num2 === 0) return null
+        else return calcDivide(num1, num2)
+      default:
+        return null
+    }
+  }
