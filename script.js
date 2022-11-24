36fbfd6f3d7a88a52c7efccbf0c12d34614856bd
Pranav Borse <pranavborse91@gmail.com>
2022-11-24 22:26:27 +1100

    Create logic to evaluate two numbers

--- script.js
+++ script.js
@@ -4,86 +4,86 @@ const inputButton = document.querySelectorAll('.inputButton');
 const divideButton = document.querySelector('.divideButton');
 const equalsButton = document.querySelector('.equalsButton');
 const operatorButton = document.querySelectorAll('[data-operator]')
+const numberButton = document.querySelectorAll('[data-num]')
 
-let result = "";
 let num1 = ""; //does nothing
 let operator = ""; //does nothing
 let num2 = "";
 
-//Calc display
 //Current display
-for(let i = 0; i < inputButton.length; i++){
-    inputButton[i].addEventListener('click', function(){
-        result = result + inputButton[i].getAttribute("data-all");
-        currentDisplay.textContent = result;  
-    })   
-    
+numberButton.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    handleNumber(e.target.textContent);
+  })
 }
+)
+function handleNumber(number){
+  if (num1.length <= 11){
+    num1 += number;
+    currentDisplay.textContent = num1;
+  }
+}
+
 //Previous display
+operatorButton.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    handleOperator(e.target.textContent);
+  })
+})
+
 function handleOperator(op){
   operator = op;
-  num2 = result;
-  prevDisplay.textContent = num2;
-  result = "";
+  num2 = num1;
+  console.log(num2)
+  prevDisplay.textContent = num2 + operator;
+  num1 = "";
   currentDisplay.textContent = "";
 }
 
-//num 1
-for(let i = 0; i < inputButton.length; i++){
-  inputButton[i].addEventListener('click', function(){
-      num1 = inputButton[i].getAttribute("data-num");
-      if(num1){
-        console.log(`num1: ${num1}`)
-      }
-  })   
-}
+equalsButton.addEventListener('click', operate);
 
-//operator
-for(let i = 0; i < operatorButton.length; i++){
-  operatorButton[i].addEventListener('click', function(e){
-      operator = operatorButton[i].getAttribute("data-operator");
-      if(operator){
-        console.log(`operator: ${operator}`)
-      }
-      handleOperator(operator)
-  })   
-}
-
-
-
-console.log(`num2: ${num2}`);
-
-equalsButton.addEventListener('click', operate());
-
+//Doing the calculation
 function calcPercent(num1, num2){
-    return num1/100 * num2
+  return (num2 / 100) * num1
 }
 function calcDivide(num1, num2){
-    return num1 / num2
+    return num2 / num1
 }
 function calcMultiply(num1, num2){
     return num1 * num2
 }
 function calcSubtract(num1, num2){
-    return num1 - num2
+    return num2 - num1
 }
 function calcAdd(num1, num2){
     return num1 + num2
 }
-function operate(operator, num1, num2) {
+function operate() {
     num1 = Number(num1)
     num2 = Number(num2)
+
     switch (operator) {
       case '+':
-        return calcAdd(num1, num2)
+        num2 = calcAdd(num1, num2)
+        break
       case '-':
-        return calcSubtract(num1, num2)
+        num2 = calcSubtract(num1, num2)
+        break
       case '×':
-        return calcMultiply(num1, num2)
+        num2 = calcMultiply(num1, num2)
+        break
+      case '%':
+        num2 = calcPercent(num1, num2)
+        break
       case '÷':
-        if (num2 === 0) return null
-        else return calcDivide(num1, num2)
-      default:
-        return null
+        num2 = num2.toString(); 
+        if (num1 == 0){
+          num2 = "Error, you can't divide by 0"
+        } else{
+          num2 = calcDivide(num1, num2);
+        }
+        break
     }
+    prevDisplay.textContent = "";
+    currentDisplay.textContent = num2;
   }
