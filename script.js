531af34b0bdf4f9e34f9c7e21bb70b1059b136bf
Pranav Borse <pranavborse91@gmail.com>
2022-11-25 17:06:49 +1100

    Fix clear button

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ const numberButton = document.querySelectorAll('[data-num]');
 const clearButton = document.querySelector('.clearButton');
 const deleteButton = document.querySelector('.deleteButton');
 
-let num1 = ""; //does nothing
-let operator = ""; //does nothing
+//Global variables
+let num1 = ""; 
+let operator = "";
 let num2 = "";
 
 //Current display
@@ -20,19 +21,22 @@ numberButton.forEach((btn) => {
 }
 )
 function handleNumber(number){
-  if (num1.length <= 11){
+  if (num1.length <= 21){
     num1 += number;
     currentDisplay.textContent = num1;
   }
+  else if (num1){
+    numberButton.addEventListener('click', function(){
+      prevDisplay.textContent = `ANS = ${num1}`
+    })
+  }
 }
-
 //Previous display
 operatorButton.forEach((btn) => {
   btn.addEventListener('click', (e) => {
     handleOperator(e.target.textContent);
   })
 })
-
 function handleOperator(op){
   operator = op;
   num2 = num1;
@@ -40,20 +44,19 @@ function handleOperator(op){
   num1 = "";
   currentDisplay.textContent = "";
 }
-
 //Clear button
 clearButton.addEventListener('click', () => {
   prevDisplay.textContent = '';
   currentDisplay.textContent = '';
+  num1 = ''
 })
 //Delete button
 deleteButton.addEventListener('click', function(){
   num1 = num1.slice(0, currentDisplay.textContent.length - 1)
   currentDisplay.textContent = currentDisplay.textContent.slice(0, currentDisplay.textContent.length - 1)
 })
-
+//Equals button
 equalsButton.addEventListener('click', operate);
-
 //Doing the calculation
 function calcPercent(num1, num2){
   return (num2 / 100) * num1
